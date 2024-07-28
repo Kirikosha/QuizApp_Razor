7de2e0154b75eb74efe01d744467793b68460b17
Kirikosha <kirikoshamain@gmail.com>
2024-07-28 17:35:30 +0200

    Completed design for quiz creation page

--- QuizApp-Razor/obj/Debug/net8.0/QuizApp-Razor.AssemblyInfo.cs
+++ QuizApp-Razor/obj/Debug/net8.0/QuizApp-Razor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp-Razor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45108e6b31ec0bafc521e6678b76ca0d7feab781")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp-Razor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp-Razor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
