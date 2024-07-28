45108e6b31ec0bafc521e6678b76ca0d7feab781
Kirikosha <kirikoshamain@gmail.com>
2024-07-28 12:15:36 +0200

    Initial commit

---
+++ QuizApp-Razor/Pages/CreateQuiz.cshtml.cs
@@ -0,0 +1,8 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace QuizApp_Razor.Pages;
+
+public class CreateQuizModel : PageModel{
+
+}
